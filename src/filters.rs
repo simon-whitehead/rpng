9a05bbf2abae138f4b287b5efe1058fd1fddede9
Simon Whitehead <chemnova@gmail.com>
2016-04-18 23:12:14 +1000

    Renamed argument

--- src/filters.rs
+++ src/filters.rs
@@ -2,25 +2,25 @@
 use png::PngFile;
 
 pub trait Filter {
-    fn apply(&self, row: &mut [u8], start: usize, png: &PngFile);
+    fn apply(&self, data: &mut [u8], start: usize, png: &PngFile);
 }
 
 pub struct None;
 impl Filter for None {
-    fn apply(&self, _row: &mut [u8], _start: usize, _png: &PngFile) {
+    fn apply(&self, _data: &mut [u8], _start: usize, _png: &PngFile) {
         // No op
     }
 }
 
 pub struct Sub;
 impl Filter for Sub {
-    fn apply(&self, row: &mut [u8], start: usize, png: &PngFile) {
+    fn apply(&self, data: &mut [u8], start: usize, png: &PngFile) {
         let mut i = 0;
         while i < png.pitch {
             let x = start + i;
             if x - start > png.bytes_per_pixel {
-                let result = row[x] as u32 + row[x - png.bytes_per_pixel] as u32;
-                row[x] = result as u8;
+                let result = data[x] as u32 + data[x - png.bytes_per_pixel] as u32;
+                data[x] = result as u8;
             }
 
             i += 1;
